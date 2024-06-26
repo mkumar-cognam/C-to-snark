5cf21a6e13ab1a178c315571d0109adda72d3160
mkumar-cognam <mkumar@cognam.com>
2024-06-26 09:33:57 +0530

    comment added rescheduled

--- test_cli.sh
+++ test_cli.sh
@@ -12,3 +12,4 @@ RUST_LOG=info cargo run --release  -- -k 18 --function zkmain --output ./output 
 RUST_LOG=info cargo run --release -- -k 18 --function zkmain --output ./output --wasm wasm/program.wasm aggregate-prove --public 11:i64
 RUST_LOG=info cargo run --release -- -k 18 --function zkmain --output ./output --wasm wasm/program.wasm aggregate-verify --proof output/aggregate-circuit.0.transcript.data  --instances output/aggregate-circuit.0.instance.data
 
+# Added some comment
